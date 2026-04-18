36327b2d67db4df0cf90cc32bf1a0dd3ff0b0f46
son0307 <162094052+son0307@users.noreply.github.com>
2026-04-18 13:12:00 +0900

    Title: 겹치지 않게 선분 고르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260418/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+class Line implements Comparable<Line>{
+    int start;
+    int end;
+
+    public Line(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int compareTo(Line o) {
+        return start - o.start;
+    }
+}
+
+public class Main {
+    static int max = Integer.MIN_VALUE;
+    static ArrayList<Line> lines = new ArrayList<>();
+    static int n;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            lines.add(new Line(start, end));
+        }
+        Collections.sort(lines);
+
+        ArrayList<Line> selectedLines = new ArrayList<>();
+        dfs(0, selectedLines);
+        System.out.println(max);
+    }
+
+    public static void dfs(int idx, ArrayList<Line> selected) {
+        if (idx == n) {
+            max = Math.max(max, selected.size());
+
+            return;
+        }
+
+        Line current = lines.get(idx);
+        boolean isAvailable = true;
+        for (Line line : selected) {
+            if (line.end >= current.start) {
+                isAvailable = false;
+                break;
+            }
+        }
+
+        if (isAvailable) {
+            selected.add(current);
+            dfs(idx + 1, selected);
+            selected.remove(selected.size() - 1);
+        } else {
+            dfs(idx + 1, selected);
+        }
+    }
+}
